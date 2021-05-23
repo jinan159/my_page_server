9e4cbdf692d55dd68684d5f5d68027d428a3de85
jinan159 <wlsdhks0423@naver.com>
2021-05-23 23:55:09 +0900

    안쓰는 의존성 제거 및 app.js 주석 정리

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const PORT = 3000;
 const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV !== 'prod' && NODE_ENV !== 'dev' && NODE_ENV !== 'dev_remote') process.env.NODE_ENV = 'dev';
 
-/**[START] 모듈 선언*/
+// 모듈 선언
 const cors = require('cors');
-/**[END] 모듈 선언*/
+const bodyParser = require('body-parser');
 
-/**[START] 모듈 적용*/
+// 모듈 적용
 app.use(cors());                                    // CORS 허용 설정
 app.use(express.json());                            // Request 데이터 json으로 변환
 // app.use(express.urlencoded({extends: true}));    // Request 데이터 json으로 변환
-/**[END] 모듈 적용*/
+app.use(bodyParser());
 
-/**[START] router 선언영역*/
-let post = require('./routes/post.js');
-/**[END] router 파일 선언영역*/
+// router 선언
+const post = require('./src/api/post/post.js'); 
 
-/**[START] router 영역*/
-app.use('/post', post);
-/**[END] router 영역*/
+// router 적용
+app.use('/api/post', post);
+
 
 //서버 start
 app.listen(PORT, function() {
