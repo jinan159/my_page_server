aa8c5d47887620c960f945dedaa3ac39fdfe9408
jinan159 <wlsdhks0423@naver.com>
2021-05-26 22:41:03 +0900

    글 목록 조회 주석 수정

--- src/services/postService.js
+++ src/services/postService.js
@@ -3,9 +3,7 @@ const postModel = require('../models/postModel');
 module.exports = {    
     /**
      * 글 목록 조회
-     * @param {String} sort 정렬방식 (ASC, DESC)
-     * @param {Number} start 페이징 시작 인덱스
-     * @param {Number} count 페이징 개수
+     * @param {Object} params       정렬 파라미터
      * @param {Function} callback 
      */
     findAllPost : (params, callback) => {
