c96395f073e2aa32bed44f0954b1d702f3d5505f
jinan159 <wlsdhks0423@naver.com>
2021-05-27 23:22:35 +0900

    errorHandler NODE_ENV 확인 로직 수정

--- utils/errors/errorHandler.js
+++ utils/errors/errorHandler.js
@@ -7,8 +7,8 @@ module.exports = (err, req, res, next) => {
     }
 
     logger.devErrorConsoleLog(err.stack);
-    
-    if (NODE_ENV !== 'dev' && NODE_ENV !== 'dev_remote') {
+
+    if (process.env.NODE_ENV !== 'dev' && process.env.NODE_ENV !== 'dev_remote') {
         return res.status(500).send(err.stack);
     } else {
         return res.status(500).send('Internal Server Error');
