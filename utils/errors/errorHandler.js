b88c3807ceaa0664dd004ab855c43ce8dcfea5a9
jinan159 <wlsdhks0423@naver.com>
2021-05-27 23:19:16 +0900

    errorHandler 개선

--- utils/errors/errorHandler.js
+++ utils/errors/errorHandler.js
@@ -1,3 +1,4 @@
+const logger = require('../logger');
 const WrongRequestError = require('./WrongRequestError');
 
 module.exports = (err, req, res, next) => {
@@ -5,5 +6,12 @@ module.exports = (err, req, res, next) => {
         return res.status(err.code).json(err);
     }
 
-    return res.status(500).json(err);
+    logger.devErrorConsoleLog(err.stack);
+    
+    if (NODE_ENV !== 'dev' && NODE_ENV !== 'dev_remote') {
+        return res.status(500).send(err.stack);
+    } else {
+        return res.status(500).send('Internal Server Error');
+    }
+    
 };
