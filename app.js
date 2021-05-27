f968eebe7c27cfcc34c9d1f86de27ac31f74dd19
jinan159 <wlsdhks0423@naver.com>
2021-05-27 23:20:14 +0900

    String에 json string 인지 확인하는 기능 추가

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+const { initializeExtends } = require('./utils/extends');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV !== 'prod' && NODE_ENV !== 'dev' && NODE_ENV !== 'dev_remote') process.env.NODE_ENV = 'dev';
 
+// 상속 정보들 초기화
+initializeExtends();
+
 // 모듈 선언
 const cors = require('cors');
 const errorHandler = require('./utils/errors/errorHandler');
+
 
 // 모듈 적용
 app.use(cors());                                    // CORS 허용 설정
 app.use(express.json());                            // Request 데이터 json으로 변환
 
 // router 선언
-const post = require('./src/api/post'); 
+const post = require('./src/api/post');
 
 // router 적용
 app.use('/api/post', post);
---
+++ utils/extends.js
@@ -0,0 +1,19 @@
+module.exports = {
+    initializeExtends: () => {
+        Object.assign(String.prototype, {
+            /**
+             * 해당 String이 JSON String 인지 여부를 반환한다.
+             * @returns 
+             */
+            isJsonString : function() {
+                try { 
+                    JSON.parse(this); 
+                } 
+                catch { 
+                    return false; 
+                }
+                return true;
+            }
+        });
+    }
+}
